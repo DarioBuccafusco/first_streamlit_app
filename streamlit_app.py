514d0f1f37fca87d01389aaa09fb0d4d0ad9a29d
Gruppo 1 <36633837+DarioBuccafusco@users.noreply.github.com>
2023-08-23 17:59:39 +0200

    Update streamlit_app.py
    adding try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,18 +27,20 @@ streamlit.dataframe(fruits_to_show)
 
 #Fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-#Normalize the json response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#Show as a dataframe
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   #streamlit.write('The user entered ', fruit_choice)
+   if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+   else
+      #import requests
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      #Normalize the json response
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      #Show as a dataframe
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+   streamlit.error()
 
 add_my_fruit = streamlit.text_input('What fruit would you want to add?','Kiwi')
 streamlit.write('The user want tot add ' , add_my_fruit)
