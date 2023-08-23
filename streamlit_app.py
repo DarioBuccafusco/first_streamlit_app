8593a30e68feb49c47ec6d0ca50cfff3a3de0497
Gruppo 1 <36633837+DarioBuccafusco@users.noreply.github.com>
2023-08-23 10:48:50 +0200

    Update streamlit_app.py
    trying to show changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #List to pick the fruit that they want
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+selected_fruits = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[selected_fruits]
 streamlit.text('😋')
 
 #display the table on the page
