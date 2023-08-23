c58890e83b31819a0d7ddb4811f556a9570ef078
Gruppo 1 <36633837+DarioBuccafusco@users.noreply.github.com>
2023-08-23 10:16:14 +0200

    Update streamlit_app.py
    added extra menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ streamlit.title('My Parents New Healthy Diner')
    
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Balueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
